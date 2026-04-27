2838344f292e677ed8ff615d2aec82775cb63c5f
dinesh-veer <dveer123@hotmail.com>
2026-04-27 09:22:09 +0530

    spring boot activemq example

---
+++ spring-boot-apache-activemq/src/main/java/com/dinesh/springbootapacheactivemq/SpringBootApacheActivemqApplication.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootapacheactivemq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApacheActivemqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApacheActivemqApplication.class, args);
+    }
+
+}
---
+++ spring-boot-apache-activemq/src/test/java/com/dinesh/springbootapacheactivemq/SpringBootApacheActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinesh.springbootapacheactivemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApacheActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
